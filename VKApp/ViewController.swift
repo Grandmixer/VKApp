e36eaa9dd51dfc6987c5258932a6f3d0803b0ece
Alexandr <AlexandrZhelanov@gmail.com>
2020-08-23 18:41:56 +0500

    Initial Commit

---
+++ VKApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VKApp
+//
+//  Created by Желанов Александр Валентинович on 23.08.2020.
+//  Copyright © 2020 OlwaStd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
